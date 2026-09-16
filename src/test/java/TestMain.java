c63f9c99e86920f9f757e5b6e7374f38ac7250ee
Your Name <you@example.com>
2026-09-16 21:14:56 +0400

    first commit

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+public class TestMain {
+    @Test
+    public static boolean isEven(int n) {
+        if (n % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+}
