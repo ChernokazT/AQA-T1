a6ce340ad4d70a54c9b5c7e03aeafe7c1aa4e758
Your Name <you@example.com>
2026-09-16 22:49:19 +0400

    Zadanie 3

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 public class TestMain {
-    @Test
+
     public static boolean isEven(int n) {
         if (n % 2 == 0){
             return true;
@@ -10,8 +10,52 @@ public class TestMain {
         }
 
     }
-    @Test
+
     public static String checkAccess(int age) {
-        return (age > 18) ? "Allowed" : "Denied";
+        if (age > 18){
+            return "Allowed";
+        }else {
+            return "Denied";
+        }
     }
+
+     public static boolean isPositive(int n) {
+         return (n >= 0) ? true : false;
+     }
+
+    public static String getGrade(int score) {
+        if (score >= 0 && score <= 20) {
+            return "E";
+        } else if (score >= 21 && score <= 40) {
+            return "D";
+        } else if (score >= 41 && score <= 60) {
+            return "C";
+        } else if (score >= 61 && score <= 80) {
+            return "B";
+        } else if (score >= 81 && score <= 100) {
+            return "A";
+        } else {
+            return "Error";
+        }
+    }
+
+    public static String blastOff(int start) {
+        int[] arr = new int[start];
+        String num = null;
+        for (int i = 1; i > 0; i--) {
+            num = String.valueOf(arr[i]);
+        }
+        return (num + " Поехали!");
+
+    }
+    public static int sumToN(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+
 }
